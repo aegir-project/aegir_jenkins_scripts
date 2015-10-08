24b07ba2412cb79cdad7c6e913e5cc94a4b32745
Herman van Rink <rink@initfour.nl>
2015-10-08 22:39:41 +0200

    cleanup

--- buildenv-debian-jessie/build_docker.sh
+++ buildenv-debian-jessie/build_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 docker build -t aegir3_build_env .
-export BUILD_NUMBER=42
-#docker run -it --volume=/srv/reprepro/incoming:/incoming aegir3_build_env 
-docker run -it --volume=/tmp/newpkg:/incoming \
+
+# Jenkins provides a build number.
+#export BUILD_NUMBER=42
+
+docker run -it
        --volume=/tmp/newpkg:/incoming \
        --volume=/var/lib/jenkins/.gnupg:/root/.gnupg:ro \
        --env=BUILD_NUMBER=$BUILD_NUMBER \
@@ -11,6 +13,3 @@ docker run -it --volume=/tmp/newpkg:/incoming \
 
 # debug
 #docker run -it aegir3_build_env /bin/bash
-
-# Cleanup
-#rm -r .gnupg
