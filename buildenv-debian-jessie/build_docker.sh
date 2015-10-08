2211cf9a39112bf794f8ef077681cbdc80bd199d
Herman van Rink <rink@initfour.nl>
2015-10-08 22:19:32 +0200

    Move gnupg keys to a volume
    Gnupg does complain about it being readonly and having incorrect permissions.

--- buildenv-debian-jessie/build_docker.sh
+++ buildenv-debian-jessie/build_docker.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-cp -rv /var/lib/jenkins/.gnupg .
-
 docker build -t aegir3_build_env .
 export BUILD_NUMBER=42
 #docker run -it --volume=/srv/reprepro/incoming:/incoming aegir3_build_env 
-docker run -it --volume=/tmp/newpkg:/incoming --env=BUILD_NUMBER=$BUILD_NUMBER aegir3_build_env
+docker run -it --volume=/tmp/newpkg:/incoming \
+       --volume=/tmp/newpkg:/incoming \
+       --volume=/var/lib/jenkins/.gnupg:/root/.gnupg:ro \
+       --env=BUILD_NUMBER=$BUILD_NUMBER \
+       aegir3_build_env
 
 # debug
 #docker run -it aegir3_build_env /bin/bash
